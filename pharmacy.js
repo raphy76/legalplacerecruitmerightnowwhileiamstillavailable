859816a2b32e4ef0f5b0cfa7075dc3c63585c7bc
raphou <raphael76530@gmail.com>
2025-10-17 04:43:36 +0200

    [Refactor Drug] Refactor of class Drug for more modularity and optimisation

--- pharmacy.js
+++ pharmacy.js
@@ -1,48 +1,48 @@
+// Compact solution
+
 export class Drug {
+  static MAX_BENEFIT = 50;
+  static MIN_BENEFIT = 0;
+
+  static NORMAL_MALUS = 1;
+  static EXPIRED_NORMAL_MALUS = 2 * Drug.NORMAL_MALUS;
+
+  static HERBAL_BONUS = 1;
+  static EXPIRED_HERBAL_BONUS = 2 * Drug.HERBAL_BONUS;
+
+  static EXPIRED_FERVEX_BENEFIT = 0;
+  static FERVEX_BONUS = 1;
+  static FERVEX_BONUS_5_DAYS = 3;
+  static FERVEX_BONUS_10_DAYS = 2;
+
+  static DAFALGAN_MALUS = 2 * Drug.NORMAL_MALUS;
+  static EXPIRED_DAFALGAN_MALUS = 2 * Drug.DAFALGAN_MALUS;
+
   constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
+
+    this.updatefunc = this.#resolveUpdateFunc(name);
   }
 
-  isExpired(){
-    return (this.expiresIn <= 0)
+  update() {
+    this.updatefunc();
   }
 
-  update(){
-    switch (this.name) {
-      case "Magic Pill":
-        return;
+  #resolveUpdateFunc(name){
+    let updatefunc;
 
-      case "Fervex":
-        if (this.isExpired()) {
-          this.benefit = 0;
-        } else {
-          let bonus = 1;
-          if (this.expiresIn <= 10) bonus = 2;
-          if (this.expiresIn <= 5) bonus = 3;
-          this.benefit += bonus;
-        }
-        break;
+    switch (name) {
+      case "Herbal Tea": updatefunc = this.#updateHerbalTea; break;
+      case "Magic Pill": updatefunc = this.#updateMagicPill; break;
+      case "Fervex": updatefunc = this.#updateFervex; break;
+      case "Dafalgan": updatefunc = this.#updateDafalgan; break;
+      
+      default: updatefunc = this.#updateNormalDrug;
+    }  
 
-      case "Herbal Tea":
-        this.benefit += this.isExpired() ? 2 : 1;
-        break;
-
-      case "Dafalgan":
-        // twice as fast as normal
-        this.benefit -= this.isExpired() ? 4 : 2;
-        break;
-        
-      default:
-        this.benefit -= this.isExpired() ? 2 : 1;
-        break;
-    }
-
-    // Clamp
-    this.benefit = Math.min(50, Math.max(0, this.benefit));
-
-    this.expiresIn--; 
+    return updatefunc;
   }
 
   getExpiresIn(){
@@ -52,15 +52,68 @@ export class Drug {
   getBenefit(){
     return this.benefit;
   }
+
+  //// Update for normal drug
+  #updateNormalDrug() {
+    const malus = this.#isExpired() ? Drug.EXPIRED_NORMAL_MALUS : Drug.NORMAL_MALUS
+    this.#setBenefit(this.benefit - malus);
+
+    this.expiresIn--;
+  }
+
+
+  //// Update for special drugs
+  #updateHerbalTea() {
+    const bonus = this.#isExpired() ? Drug.EXPIRED_HERBAL_BONUS : Drug.HERBAL_BONUS
+    this.#setBenefit(this.benefit + bonus);
+
+    this.expiresIn--;
+  }
+
+  #updateMagicPill(){
+    // No change
+  }
+
+  #updateFervex() {
+    if(this.#isExpired()){
+      this.benefit = Drug.EXPIRED_FERVEX_BENEFIT;
+    } else {
+      let bonus = Drug.FERVEX_BONUS
+      if(this.expiresIn <= 10) bonus = Drug.FERVEX_BONUS_10_DAYS;
+      if(this.expiresIn <= 5) bonus = Drug.FERVEX_BONUS_5_DAYS;
+      
+      this.#setBenefit(this.benefit + bonus);
+    }
+
+    this.expiresIn--;
+  }
+
+  #updateDafalgan() {
+    const malus = this.#isExpired() ? Drug.EXPIRED_DAFALGAN_MALUS : Drug.DAFALGAN_MALUS
+    this.#setBenefit(this.benefit - malus);
+
+    this.expiresIn--;
+  }
+
+
+  //// Utility methods
+  #isExpired(){
+    return (this.expiresIn <= 0);
+  }
+
+  #setBenefit(v){
+    this.benefit = Math.min(Drug.MAX_BENEFIT, Math.max(Drug.MIN_BENEFIT, v));
+  }
 }
 
 export class Pharmacy {
+
   constructor(drugs = []) {
     this.drugs = drugs;
   }
 
   updateBenefitValue() {
-    for(let drug of this.drugs){
+    for (const drug of this.drugs) {
       drug.update()
     }
 
